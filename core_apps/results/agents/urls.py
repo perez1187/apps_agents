d14ed8b4f98753e3c77e617b536263e8a824572c
perez1187 <o.perez1187@gmail.com>
2024-03-11 16:10:15 +0100

    agent player list and adding players to agent

--- core_apps/results/agents/urls.py
+++ core_apps/results/agents/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import AgentListView
+from .views import AgentListView,AddPlayer
 
 urlpatterns = [
     # path('upload/', UploadFileView.as_view(), name='upload-file'),
     path('player-list/',AgentListView.as_view(),name="testres"),
+    path('add-player',AddPlayer.as_view())
     # path('add-report-manual/',UploadFileView.as_view(),name="testres2"),
     # path('agent-result-summary/',PlayerResultsView.as_view(),name="player_results"),
     # path('agent-results-details/', Agent_raw_results_View.as_view(), name='upload-file'),
--- core_apps/results/agents/views.py
+++ core_apps/results/agents/views.py
@@ -1,12 +1,21 @@
 from rest_framework.response import Response
+# from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 from rest_framework import status, generics, filters, permissions
+
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
 
 from core_apps.users.profiles.models import Profile
 from . import serializers
+# from core import models
+from .permissions import IsAgent
+
 
 
 class AgentListView(generics.ListAPIView):
-    permission_classes = [permissions.IsAuthenticated]    
+    permission_classes = [permissions.IsAuthenticated,IsAgent]    
     serializer_class = serializers.AgentListSeriaizer
 
 
@@ -37,3 +46,35 @@ class AgentListView(generics.ListAPIView):
     #     }          
 
         return Response(results,status.HTTP_200_OK)          
+
+class AddPlayer(APIView):
+    permission_classes = [permissions.IsAuthenticated,IsAgent]
+
+    def post(self, request, format=None):
+        print("taak")
+        # uid = "b939361e-cc4f-49b6-b386-67bc9364e8a8"
+        if request.data["id"] :
+            
+            try:
+                find_user = User.objects.get(id=request.data["id"])
+            except:
+                return Response({"error":"Player doesnt exist"},status=status.HTTP_404_NOT_FOUND)
+            # print(find_user[0].pkid) # if we .filter
+            print(find_user.pk)
+            user_profile =Profile.objects.get(user = find_user.pk)
+            print(user_profile.agent)
+            print(request.user)
+            user_profile.agent=request.user
+            user_profile.save()
+
+        print(request.data["id"])
+
+        return Response("yes",status=status.HTTP_201_CREATED)
+
+        # serializer = SnippetSerializer(data=request.data)
+        # if serializer.is_valid():
+        #     serializer.save()
+        #     return Response(serializer.data, status=status.HTTP_201_CREATED)
+        # return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
