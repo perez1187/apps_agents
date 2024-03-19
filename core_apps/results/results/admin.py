f9716ddf1f219c8c3c0f15ba156b59783b6233a1
perez1187 <o.perez1187@gmail.com>
2024-03-19 14:58:36 +0100

    results model

--- core_apps/results/results/admin.py
+++ core_apps/results/results/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import Results
 
-# Register your models here.
+class ResultsAdmin(admin.ModelAdmin):
+    list_display = [
+        "id",
+        "report", 
+        # "player",
+        "nickname",
+        "nickname_id",
+        "agents",
+        "club",
+        "profit_loss",
+        "rake",
+        "agent_deal",
+        "agent_rb",
+        "agent_adjustment",
+        "agent_settlement",
+        "player_deal_rb",
+        "player_deal_adjustment",
+        "created_at",
+        "updated_at"
+        ]
+    list_display_links = ["nickname"]
+    # search_fields = ["user__username"]
+
+admin.site.register(Results, ResultsAdmin)
