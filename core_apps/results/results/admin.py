659e36f2b07b963f14f4c8187fbc6ffd32bd4b62
perez1187 <o.perez1187@gmail.com>
2024-06-03 18:20:16 +0200

    fix bug download report

--- core_apps/results/results/admin.py
+++ core_apps/results/results/admin.py
@@ -18,31 +18,30 @@ def export_results(modeladmin, request, queryset):
     writer = csv.writer(response)
     writer.writerow([
         "report", 
-        # "CLUB",
         "player",
-        "nickname",
-        "nickname_id",
-        # 
-        "club",
-        "agent_settlement",
-        "profit_loss",
-        "rake",
-        "agent_deal",
-        "agent_rb",        
-        "agent_adjustment",                
+        "CLUB",
+        "PLAYERS",
+        "NICKNAME",
+        "PROFIT/LOSS",
+        "RAKE",
+        " DEAL",
+        "RAKEBACK",
+        "ADJUSTMENT",
+        "AGENT SETTLEMENT",
+               
         ])
     results = queryset.values_list(
         "report__report_date", 
         "nickname_fk__player__username",
-        "nickname",
-        "nickname_id",
         "club",
-        "agent_settlement",
+        "nickname_id",
+        "nickname",
         "profit_loss",
         "rake",
-        "agent_deal",
-        "agent_rb",        
-        "agent_adjustment", 
+        "player_deal_rb",
+        "player_rb",
+        "player_adjustment",
+        "player_settlement",
 
         )
 
